143725bcb0452bbce72b230a8501432ef048afc4
Jared Baur <jaredbaur@fastmail.com>
2023-06-13 11:30:45 -0700

    optee-os: init at 4.4.0
    This change adds two builds of OP-TEE OS (qemu armv7 & qemu aarch64) as well
    as a function that can be used for building OP-TEE OS for any board.
    This follows the pattern similar to buildUBoot and
    buildArmTrustedFirmware.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8487,6 +8487,12 @@ with pkgs;
     libiberty_static = libiberty.override { staticBuild = true; };
   };
 
+  inherit (callPackage ../misc/optee-os { })
+    buildOptee
+    opteeQemuArm
+    opteeQemuAarch64
+    ;
+
   pactorio = callPackage ../development/tools/pactorio {
     inherit (darwin.apple_sdk.frameworks) Security;
   };
